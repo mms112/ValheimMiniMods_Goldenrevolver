8eccb9d22750ac92d667ba9ccca9f5766c05d7d1
mms112 <mms112@users.noreply.github.com>
2025-08-05 22:47:45 +0200

    Adapted the ObtainableStonePickaxe mod to the custom build system.

--- ObtainableStonePickaxe/ObtainableStonePickaxe/ObtainableStonePickaxePlugin.cs
+++ ObtainableStonePickaxe/ObtainableStonePickaxe/ObtainableStonePickaxePlugin.cs
@@ -1,16 +1,21 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using ServerSync;
 using System.Reflection;
 
 namespace ObtainableStonePickaxe
 {
-    [BepInPlugin("goldenrevolver.ObtainableStonePickaxePlugin", NAME, VERSION)]
+    [BepInPlugin(GUID, NAME, VERSION)]
     public class ObtainableStonePickaxePlugin : BaseUnityPlugin
     {
         public const string NAME = "Obtainable Stone Pickaxe";
         public const string VERSION = "1.0.1";
+        public const string GUID = "goldenrevolver.ObtainableStonePickaxePlugin";
 
+        internal static readonly ConfigSync configSync = new ConfigSync(GUID) { DisplayName = NAME, CurrentVersion = VERSION, MinimumRequiredVersion = VERSION };
+
+        private static ConfigEntry<bool> configLocked;
         public static ConfigEntry<bool> UpgradeableStonePickaxe;
         public static ConfigEntry<bool> UpgradeableAntlerPickaxe;
         public static ConfigEntry<bool> BronzePickaxeUpgradeFix;
@@ -26,22 +31,40 @@ namespace ObtainableStonePickaxe
 
         private void LoadConfig()
         {
-            //var sectionName = "0 - General";
+            configLocked = config("0 - General", "LockConfiguration", true, "Configuration is locked and can be changed by server admins only.");
+            configSync.AddLockingConfigEntry(configLocked);
 
             var sectionName = "1 - Upgrading";
 
-            BronzePickaxeUpgradeFix = Config.Bind(sectionName, nameof(BronzePickaxeUpgradeFix), true, "Whether to fix that the bronze pickaxe gains one less pickaxe damage than pierce damage per level, unlike all other pickaxes.");
-            UpgradeableAntlerPickaxe = Config.Bind(sectionName, nameof(UpgradeableAntlerPickaxe), true, "Enables upgrading of the antler pickaxe with the default upgrade formula.");
-            UpgradeableStonePickaxe = Config.Bind(sectionName, nameof(UpgradeableStonePickaxe), true, "Enables upgrading of the stone pickaxe with the default upgrade formula.");
+            BronzePickaxeUpgradeFix = config(sectionName, nameof(BronzePickaxeUpgradeFix), true, "Whether to fix that the bronze pickaxe gains one less pickaxe damage than pierce damage per level, unlike all other pickaxes.");
+            UpgradeableAntlerPickaxe = config(sectionName, nameof(UpgradeableAntlerPickaxe), true, "Enables upgrading of the antler pickaxe with the default upgrade formula.");
+            UpgradeableStonePickaxe = config(sectionName, nameof(UpgradeableStonePickaxe), true, "Enables upgrading of the stone pickaxe with the default upgrade formula.");
 
             sectionName = "9 - Debugging";
 
-            EnableDebugMessages = Config.Bind(sectionName, nameof(EnableDebugMessages), false, "Enable this if you can't break a rock with the stone pickaxe to see why the mod thinks it's not a rock. Tell this to the mod author if you think it's a bug.");
+            EnableDebugMessages = config(sectionName, nameof(EnableDebugMessages), false, "Enable this if you can't break a rock with the stone pickaxe to see why the mod thinks it's not a rock. Tell this to the mod author if you think it's a bug.");
         }
 
         internal static void DebugLog(string message)
         {
             UnityEngine.Debug.Log($"{NAME}: {message}");
         }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription = new ConfigDescription(
+                description.Description +
+                (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                description.AcceptableValues, description.Tags);
+
+            ConfigEntry<T> configEntry = Config.Bind(group, name, defaultValue, extendedDescription);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, string description, bool synchronizedSetting = true) => config(group, name, defaultValue, new ConfigDescription(description), synchronizedSetting);
     }
 }
